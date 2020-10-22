bc635c09aed7b8a17f6770d0581e2a4579bf6b1c
Kropeczka17 <71440587+Kropeczka17@users.noreply.github.com>
2020-10-22 13:51:53 +0200

    Create listing244.js

---
+++ listing244.js
@@ -0,0 +1,9 @@
+const pytanieIOdpowiedz = [
+    ["Gdziekolwiek", "Edward Stachura"],
+    ["Wyspa", "Jonasz Kofta"],
+    ["Życie", "Jan twardowski"]
+];
+
+for(let i=0; i<pytanieIOdpowiedz.length; i++){
+    console.log("tytul wiersza: " + pytanieIOdpowiedz[i][0]);
+}
