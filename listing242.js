172cafbc84a67ae0537f22293f0de8c8a4a6ddb7
Kropeczka17 <71440587+Kropeczka17@users.noreply.github.com>
2020-10-22 13:51:50 +0200

    Create listing242.js

---
+++ listing242.js
@@ -0,0 +1,5 @@
+const streets = ["Aleja wolności", "Sikorskiego", "Jana z Głogowa", "Keplera"];
+//console.log(typeof(streets));
+for(let i=0; i  < streets.lenght ; i++){
+    console.log(ulice[i]);
+}
